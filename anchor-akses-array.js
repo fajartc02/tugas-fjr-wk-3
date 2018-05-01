30425247fd2aeee0e519cffd07f55e5e0ffc75e5
fajartc02 <fajartc02@gmail.com>
2018-05-01 15:08:02 +0700

    submit exercisse array2

---
+++ anchor-akses-array.js
@@ -0,0 +1,13 @@
+function balikString(word) {
+    var arr = word.split('');
+    var lengthWord = word.length - 1;
+    for (var i = 0; i <= 1; i++) {
+        var container = '';
+        for(var j = lengthWord; j >= 0; j--) {
+            container += word[j]
+        }
+    }
+    return container;
+}
+
+console.log(balikString('hello world!'));

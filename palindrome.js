e1b0102bf12f0ee89fea9b5f6267667893351ac7
fajartc02 <fajartc02@gmail.com>
2018-05-02 17:34:48 +0700

    submit palindrome

---
+++ palindrome.js
@@ -0,0 +1,29 @@
+function palindrome(kata) {
+    // you can only write your code here!
+    var lengKata = kata.length - 1 ; 
+    var container = '';
+    var state;
+    for ( var i = 0; i <= lengKata; i++) {
+        container = container + kata[i]
+    }
+    container2 = ''
+    for (var j = lengKata; j >= 0; j--){
+        container2 = container2 + kata[j]
+    }
+    if ( container === container2) {
+        state = true;
+    } else {
+        state = false;
+    }
+    return state;
+}
+//   }
+//   console.log(container)
+//   console.log(container2)
+  
+  // TEST CASES
+  console.log(palindrome('katak')); // true
+  console.log(palindrome('blanket')); // false
+  console.log(palindrome('civic')); // true
+  console.log(palindrome('kasur rusak')); // true
+  console.log(palindrome('mister')); // false

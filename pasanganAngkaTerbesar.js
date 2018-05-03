cbd4ed8194f6fa83b08417170a4a8de52814af95
fajartc02 <fajartc02@gmail.com>
2018-05-03 15:01:11 +0700

    submit pasanganAngka.js

--- pasanganAngkaTerbesar.js
+++ pasanganAngkaTerbesar.js
@@ -1,12 +1,22 @@
 function pasanganTerbesar(num) {
-  // you can only write your code here!
-  var splitNum = num.split('')
-  console.log(splitNum)
-}
-
-// TEST CASES
-// console.log(pasanganTerbesar(641573)); // 73
-// console.log(pasanganTerbesar(12783456)); // 83
-// console.log(pasanganTerbesar(910233)); // 91
-// console.log(pasanganTerbesar(71856421)); // 85
-// console.log(pasanganTerbesar(79918293)); // 99
+    // you can only write your code here!
+    var strNum = String(num);
+    var arr = [];
+    var lengNum = strNum.length - 2 // banyak strNum = 6 index dimulai dari 0
+    for ( var i = 0; i <= lengNum; i++ ) {
+      var double = strNum[i] + strNum[i + 1]
+      arr.push(double)
+    }
+    arr.sort(function (a, b) {
+      return a < b
+    })
+    var pasanganBesar = arr[0]
+    return parseInt(pasanganBesar)
+  }
+  
+  // TEST CASES
+  console.log(pasanganTerbesar(641573)); // 73
+  console.log(pasanganTerbesar(12783456)); // 83
+  console.log(pasanganTerbesar(910233)); // 91
+  console.log(pasanganTerbesar(71856421)); // 85
+  console.log(pasanganTerbesar(79918293)); // 99
